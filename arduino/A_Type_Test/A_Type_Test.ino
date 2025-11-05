43b6b40c198a286f80680f41a4d98eb28de1931c
pashiran <pashiran@gmail.com>
2025-11-05 15:47:07 +0900

    Add Arduino test code for A, B, and C Type Servo protocols

---
+++ arduino/A_Type_Test/A_Type_Test.ino
@@ -0,0 +1,12 @@
+/*
+ * A Type Servo Test
+ * - CAN 통신으로 서보 ID 설정 및 위치 제어 테스트
+ */
+
+void setup() {
+  // CAN 통신 초기화 및 서보 ID 스캔
+}
+
+void loop() {
+  // 서보 위치 명령 전송 및 응답 확인
+}
---
+++ arduino/B_Type_Test/B_Type_Test.ino
@@ -0,0 +1,12 @@
+/*
+ * B Type Servo Test
+ * - UART 통신으로 서보 각도 제어 및 토크 설정 테스트
+ */
+
+void setup() {
+  // Serial 통신 초기화 및 서보 연결 확인
+}
+
+void loop() {
+  // 각도 명령 전송 및 피드백 수신
+}
